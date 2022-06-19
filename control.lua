417c31b005d915ac0b18408822708fadef319196
Jack O'Connor <oconnor663@gmail.com>
2022-06-18 22:26:00 -0700

    more free technologies

--- control.lua
+++ control.lua
@@ -1,10 +1,24 @@
 --control.lua
 
 script.on_event(defines.events.on_player_created, function(e)
-    game.players[e.player_index].force.technologies["worker-robots-speed-1"].researched = true
-    game.players[e.player_index].force.technologies["worker-robots-speed-2"].researched = true
-    game.players[e.player_index].force.technologies["worker-robots-speed-3"].researched = true
-    game.players[e.player_index].force.technologies["worker-robots-speed-4"].researched = true
-    game.players[e.player_index].force.technologies["worker-robots-speed-5"].researched = true
-    game.players[e.player_index].force.technologies["worker-robots-speed-6"].level = 15
+  local free_tech = {
+    "belt-immunity-equipment",
+    "cliff-explosives",
+    "exoskeleton-equipment",
+    "night-vision-equipment",
+    "fusion-reactor-equipment",
+    "personal-roboport-mk2-equipment",
+  }
+
+
+  for _, name in ipairs(free_tech) do
+    game.players[e.player_index].force.technologies[name].researched = true
+  end
+
+  game.players[e.player_index].force.technologies["worker-robots-speed-1"].researched = true
+  game.players[e.player_index].force.technologies["worker-robots-speed-2"].researched = true
+  game.players[e.player_index].force.technologies["worker-robots-speed-3"].researched = true
+  game.players[e.player_index].force.technologies["worker-robots-speed-4"].researched = true
+  game.players[e.player_index].force.technologies["worker-robots-speed-5"].researched = true
+  game.players[e.player_index].force.technologies["worker-robots-speed-6"].level = 15
 end)
